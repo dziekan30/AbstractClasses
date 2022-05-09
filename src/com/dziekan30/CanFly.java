056a92b03df94ef1b7b82f4221ff5d89bafd9e65
dziekan30 <dziekan30@gmail.com>
2022-05-09 11:12:13 -0500

    Abstract Classes

---
+++ src/com/dziekan30/CanFly.java
@@ -0,0 +1,5 @@
+package com.dziekan30;
+
+public interface CanFly {
+    void fly();
+}
---
+++ src/com/dziekan30/Parrot.java
@@ -0,0 +1,9 @@
+package com.dziekan30;
+
+public class Parrot extends Bird {
+
+    public Parrot(String name) {
+        super(name);
+    }
+
+}
